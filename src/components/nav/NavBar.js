826aaff5410303e04e0a13a224d0562a4d879bd2
Noah Taggart <noahtag@gmail.com>
2022-04-18 14:53:40 -0500

    fixed error where useHistory was not imported into navbar.js

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,6 +1,8 @@
 import React from "react"
+import { useHistory } from "react-router-dom"
 import { Link } from "react-router-dom"
 import "./NavBar.css"
+
 
 export const NavBar = () => {
     const history = useHistory()
